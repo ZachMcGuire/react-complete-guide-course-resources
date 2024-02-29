b012ba9db3cef50fb328d974385642ad0eabc5dc
ZachMcGuire <zach.mcguire09@gmail.com>
2024-02-29 10:53:36 -1000

    started building React project

--- code/03 React Essentials/01-starting-project/src/App.jsx
+++ code/03 React Essentials/01-starting-project/src/App.jsx
@@ -1,15 +1,41 @@
+import { CORE_CONCEPTS } from './data.js';
+import Header from './components/Header/Header.jsx';
+import CoreConcept from './components/CoreConcept.jsx';
+import TabButton from './components/TabButton.jsx';
+
+
 function App() {
+  function handleSelect () {
+    console.log("Hello World = selected")
+}
+
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      <Header />
       <main>
+        <section id='core-concepts'>
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+            title = {CORE_CONCEPTS[0].title}
+            description={CORE_CONCEPTS[0].description}
+            image= {CORE_CONCEPTS[0].image}
+            />
+            <CoreConcept {...CORE_CONCEPTS[1]}/>
+            <CoreConcept {...CORE_CONCEPTS[2]}/>
+            <CoreConcept {...CORE_CONCEPTS[3]}/>
+          </ul>
+        </section>
+        <section id='examples'>
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect = {handleSelect}>Component</TabButton>
+            <TabButton onSelect = {handleSelect}>JSX</TabButton>
+            <TabButton onSelect = {handleSelect}>Props</TabButton>
+            <TabButton onSelect = {handleSelect}>State</TabButton>
+          </menu>
+          
+        </section>
         <h2>Time to get started!</h2>
       </main>
     </div>
